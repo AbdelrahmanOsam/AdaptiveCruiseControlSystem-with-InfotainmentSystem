09b02c5e30cb5efdb6a32210410ba138952fbf5b
ahmedsaeid <ahmedsaeid.ahme@gmail.com>
2023-06-17 13:28:51 +0300

    deiver for control

--- Moter_Driver/Driver_progarmme.c
+++ Moter_Driver/Driver_progarmme.c
@@ -8,23 +8,31 @@ Author                     Data          Version
 Ahmed saeid abdalla       15,jun          v1.0
 *******************************************************************************/
 
-#include " STD_TYPES.h"
-#include " BIT_MATH.h"
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
 
+#include "GPIO_interface.h"
 #include "Driver_interface.h"
 #include "Driver_private.h"
 #include "Driver_configer.h"
-#include "GPIO_interface.h"
-
 
 
 /*
+                    M2 -->>    ***************    <<--M4 
+                               ***************
+                               ***************
+                               ***************
+                               ***************
+                    M1 -->>    ***************    <<--M3   
+                        
 
-*PIN0 Left Moter
-*PIN1 Left Moter Revese  
-*PIN3 Righ Moter 
-*PIN4 Righ Moter Revese
+* Made THE IN1 Control THE M1 -->> Left Moter Revese   and IN2 CONTROL M2 -->> LIFT Moter 
+* Made THE IN3 Control THE M3 -->> Righ Moter Revese   and IN4 CONTROL M4 -->> RIGHT Moter 
 
+*PIN0 -->>>>>>>  
+*PIN1 -->>>>>>> 
+*PIN2 -->>>>>>> 
+*PIN3 -->>>>>>> 
 
 */
 
@@ -36,41 +44,50 @@ void Moter_VoidCarInit(void)
     MGPIO_voidSetPinMode(GPIO_PORTA,GPIO_PIN2,GPIO_MODE_OUTPUT);
     MGPIO_voidSetPinMode(GPIO_PORTA,GPIO_PIN3,GPIO_MODE_OUTPUT);
     /***********************  Made The Car Go Forword In Mediuem Speed **********************************************************/
-    MGPIO_voidSetOutputPinSpeed(GPIO_PORTA,GPIO_PIN0,GPIO_MEDIUM_SPEED);
-    MGPIO_voidSetOutputPinSpeed(GPIO_PORTA,GPIO_PIN1,GPIO_MEDIUM_SPEED);
-    MGPIO_voidSetOutputPinSpeed(GPIO_PORTA,GPIO_PIN2,GPIO_MEDIUM_SPEED);
-    MGPIO_voidSetOutputPinSpeed(GPIO_PORTA,GPIO_PIN3,GPIO_MEDIUM_SPEED);
 
 }
 
 void Moter_VoidCarForword(void)
 {
+    /********************* Control Moter Using H_Bridge ***********************************/
   /*********************** Make The OUTPUT HIGH TO Move The Car In Forward Direction ****************************************************************/
-    MGPIO_voidSetPinValue(GPIO_PORTA,GPIO_PIN0,GPIO_HIGH);
-    MGPIO_voidSetPinValue(GPIO_PORTA,GPIO_PIN3,GPIO_HIGH);
+    MGPIO_voidSetPinValue(GPIO_PORTA,GPIO_PIN0,GPIO_HIGH);    // Put IN1 High  
+    MGPIO_voidSetPinValue(GPIO_PORTA,GPIO_PIN1,GPIO_LOW);     // Put IN2 LOW 
+    MGPIO_voidSetPinValue(GPIO_PORTA,GPIO_PIN2,GPIO_HIGH);    // Put IN3 High  
+    MGPIO_voidSetPinValue(GPIO_PORTA,GPIO_PIN3,GPIO_LOW);     // Put IN4 LOW 
 }
 void Moter_VoidCarBackword(void)
 {
-   
-    MGPIO_voidSetPinValue(GPIO_PORTA,GPIO_PIN1,GPIO_HIGH);
-    MGPIO_voidSetPinValue(GPIO_PORTA,GPIO_PIN3,GPIO_HIGH);
+   /******************************  Made The Car Go The Reverse Direaction  *********************************************/
+    MGPIO_voidSetPinValue(GPIO_PORTA,GPIO_PIN1,GPIO_LOW);   // PUT IN1 LOW 
+    MGPIO_voidSetPinValue(GPIO_PORTA,GPIO_PIN3,GPIO_HIGH);  // PUT IN2 HIGH
+    MGPIO_voidSetPinValue(GPIO_PORTA,GPIO_PIN1,GPIO_LOW);   // PUT IN3 LOW 
+    MGPIO_voidSetPinValue(GPIO_PORTA,GPIO_PIN3,GPIO_HIGH);  // PUT IN4 HIGH
 
 }
 void Moter_VoidCarStop(void)
 {
-   MGPIO_voidSetPinValue(GPIO_PORTA,GPIO_PIN0,GPIO_LOW);
-   MGPIO_voidSetPinValue(GPIO_PORTA,GPIO_PIN1,GPIO_LOW);
-   MGPIO_voidSetPinValue(GPIO_PORTA,GPIO_PIN2,GPIO_LOW);
-   MGPIO_voidSetPinValue(GPIO_PORTA,GPIO_PIN3,GPIO_LOW);
+    /******************************* TO Stop The Car *************************************/
+   MGPIO_voidSetPinValue(GPIO_PORTA,GPIO_PIN0,GPIO_LOW);         // PUT The IN1 LOW
+   MGPIO_voidSetPinValue(GPIO_PORTA,GPIO_PIN1,GPIO_LOW);        // PUT The IN2 LOW
+   MGPIO_voidSetPinValue(GPIO_PORTA,GPIO_PIN0,GPIO_LOW);       // PUT The IN3 LOW
+   MGPIO_voidSetPinValue(GPIO_PORTA,GPIO_PIN1,GPIO_LOW);      // PUT The IN4 LOW
 }
 void Moter_VoidCarRight(void)
 {
-   MGPIO_voidSetPinValue(GPIO_PORTA,GPIO_PIN0,GPIO_HIGH);
-
+    /******************************* TO MADE The Car GO Right *************************************/
+   MGPIO_voidSetPinValue(GPIO_PORTA,GPIO_PIN0,GPIO_HIGH);         // PUT IN1 HIGH M1
+   MGPIO_voidSetPinValue(GPIO_PORTA,GPIO_PIN1,GPIO_LOW);         // PUT IN2 LOW  M1
+   MGPIO_voidSetPinValue(GPIO_PORTA,GPIO_PIN2,GPIO_HIGH);       // PUT IN1 HIGH M2
+   MGPIO_voidSetPinValue(GPIO_PORTA,GPIO_PIN3,GPIO_LOW);       // PUT IN2 LOW  M2
 
 }
 void Moter_VoidCarLeft(void)
 {
-     MGPIO_voidSetPinValue(GPIO_PORTA,GPIO_PIN3,GPIO_HIGH);
+    /******************************* TO MADE The Car GO Lift  *************************************/
+   MGPIO_voidSetPinValue(GPIO_PORTA,GPIO_PIN0,GPIO_LOW);           // PUT IN1 HIGH M1
+   MGPIO_voidSetPinValue(GPIO_PORTA,GPIO_PIN1,GPIO_HIGH);         // PUT IN2 LOW  M1
+   MGPIO_voidSetPinValue(GPIO_PORTA,GPIO_PIN2,GPIO_LOW);         // PUT IN1 HIGH M2
+   MGPIO_voidSetPinValue(GPIO_PORTA,GPIO_PIN3,GPIO_HIGH);       // PUT IN2 LOW  M2
 }
 
