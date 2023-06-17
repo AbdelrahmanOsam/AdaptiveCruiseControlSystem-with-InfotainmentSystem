8f9c55b960bbfb285ea3c2706de3bdf53cc1c1af
ahmedsaeid <ahmedsaeid.ahme@gmail.com>
2023-06-17 13:25:08 +0300

    moter_driver

--- CONTROL_CAR/main.c
+++ CONTROL_CAR/main.c
@@ -11,38 +11,42 @@ Ahmed saeid abdalla       15,jun          v1.0             STM32F401CC
 
 #include "USART_interface.h"
 #include "Driver_interface.h"
+#include "RCC_interface.h"
 
 
 void main (void)
 {
      Moter_VoidCarInit();
      MUSART_voidInit();
-    
+     MRCC_voidInit();
+     MRCC_voidEnablePeripheralClock(RCC_APB2,RCC_USART1);
+           
+           u8 Recive_u8_Data;
 
-        if (MUSART_u8ReceiveDataSynchronous == 'F')
+        if ( Recive_u8_Data = MUSART_u8ReceiveDataSynchronous == 'F')
         {
             
              Moter_VoidCarForword();
 
         }
 
-        else if (MUSART_u8ReceiveDataSynchronous == 'B')
+        else if (Recive_u8_Data = MUSART_u8ReceiveDataSynchronous == 'B')
         {
              Moter_VoidCarBackword();
         }
 
-         else if (MUSART_u8ReceiveDataSynchronous == 'R')
+         else if (Recive_u8_Data = MUSART_u8ReceiveDataSynchronous == 'R')
         {
             Moter_VoidCarRight();
         }
 
-         else if (MUSART_u8ReceiveDataSynchronous == 'L')
+         else if (Recive_u8_Data = MUSART_u8ReceiveDataSynchronous == 'L')
         {
              Moter_VoidCarLeft();
 
         }
 
-         else if (MUSART_u8ReceiveDataSynchronous == 'S')
+         else if (Recive_u8_Data = MUSART_u8ReceiveDataSynchronous == 'S')
         {
             Moter_VoidCarStop();
 
